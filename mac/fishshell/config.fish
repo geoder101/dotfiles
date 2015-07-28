bba86edf43fa508ea6d4491aaa47e8441e6843f4
George Dernikos <geoder101@gmail.com>
2015-07-28 23:15:44 +0300

    add mac/fishshell

---
+++ mac/fishshell/config.fish
@@ -0,0 +1,9 @@
+alias l ls
+alias vim /usr/local/bin/vim
+
+set -U fish_user_abbreviations 'g=git'
+
+set -x DOCKER_HOST tcp://192.168.59.103:2376
+set -x DOCKER_CERT_PATH /Users/geoder101/.boot2docker/certs/boot2docker-vm
+set -x DOCKER_TLS_VERIFY 1
+
