5476880263c3da2e1389578155cff5e13901977e
George Dernikos <geoder101@gmail.com>
2015-08-14 23:01:25 +0300

    dynamically setup docker env variables

--- mac/fishshell/config.fish
+++ mac/fishshell/config.fish
@@ -9,7 +9,4 @@ abbr -a dkc=docker-compose
 abbr -a dkm=docker-machine
 abbr -a dkr=docker run -it --rm
 
-set -x DOCKER_TLS_VERIFY "1"
-set -x DOCKER_HOST "tcp://192.168.99.100:2376"
-set -x DOCKER_CERT_PATH "/Users/geoder101/.docker/machine/machines/default"
-set -x DOCKER_MACHINE_NAME "default"
+eval (docker-machine env default --shell fish)
