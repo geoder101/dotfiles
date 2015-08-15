8e0441cad70129ae768b0a3146f2ee580158cbf9
George Dernikos <geoder101@gmail.com>
2015-08-15 12:38:00 +0300

    conditionally setup docker-machine env vars

--- mac/fishshell/config.fish
+++ mac/fishshell/config.fish
@@ -9,4 +9,9 @@ abbr -a dkc=docker-compose
 abbr -a dkm=docker-machine
 abbr -a dkr=docker run -it --rm
 
-eval (docker-machine env default --shell fish) &
+set docker_machine default
+
+switch (docker-machine status $docker_machine)
+    case Running
+        eval (docker-machine env $docker_machine --shell fish)
+end
