eb783476441ca6ec883470dcf1769ea5f31ccad6
George Dernikos <geoder101@gmail.com>
2016-05-05 12:01:43 +0300

    setup ~/.oh-my-zsh/custom/gopath.zsh

--- mac/setup.sh
+++ mac/setup.sh
@@ -3,3 +3,4 @@
 ln -sf $PWD/.profile ~/.profile
 ln -sf $PWD/docker-start.sh ~/docker-start.sh
 ln -sf $PWD/oh-my-zsh/custom/docker-env.zsh ~/.oh-my-zsh/custom/docker-env.zsh
+ln -sf $PWD/oh-my-zsh/custom/gopath.zsh ~/.oh-my-zsh/custom/gopath.zsh
