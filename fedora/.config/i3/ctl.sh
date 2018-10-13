3ce9b37d5e7d8d4e1ecbcf8ca6b0d225a4e413c3
George Dernikos <geoder101@gmail.com>
2018-10-13 20:53:47 +0300

    Refactor
    Turn on numlock on init
    
    Start 'pavucontrol' on init

--- fedora/.config/i3/ctl.sh
+++ fedora/.config/i3/ctl.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
+
+init() {
+    feh --bg-scale ~/Pictures/Matrix_Code.png
+    for i in {0..1}; do pactl set-sink-volume $i 60%; done
+    numlockx on
+    i3-msg 'workspace ctl; exec pavucontrol; workspace 2'
+}
+
 lock() {
     i3lock --color '#1f222d'
 }
 
 case "$1" in
+    init)
+        init
+        ;;
     lock)
         lock
         ;;
     logout)
         i3-msg exit
+        ;;
+    exit)
+        i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit'
         ;;
     suspend)
         lock && systemctl suspend
@@ -26,8 +40,17 @@ case "$1" in
         [ -d ~/Pictures/Screenshots ] || mkdir ~/Pictures/Screenshots
         import ~/Pictures/Screenshots/Screenshot_$(date +%Y-%m-%d-%H-%M-%S).png
         ;;
+    volup)
+        for sink in {0..1}; do pactl set-sink-volume $sink +5%; done
+        ;;
+    voldown)
+        for sink in {0..1}; do pactl set-sink-volume $sink -5%; done
+        ;;
+    volmute)
+        for sink in {0..1}; do pactl set-sink-mute $sink toggle; done
+        ;;
     *)
-        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown|screenshot}"
+        echo "Usage: $0 <cmd>"
         exit 2
 esac
 
